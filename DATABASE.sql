60f083a049b5f4a78a480a986e709afa913b9d43
Justin Muris <1736302muris@zuyd.nl>
2019-01-21 14:45:32 +0100

    New database SQL script

--- DATABASE.sql
+++ DATABASE.sql
@@ -1,83 +1,11 @@
 -- Installatiescript
 
--- Create enum namespace
-CREATE TABLE [EnumNamespace](
-[Id] INT IDENTITY(1, 1),
-[Name] NVARCHAR(100) NOT NULL,
-CONSTRAINT [PK_EnumNamespace] PRIMARY KEY ([Id]),
-CONSTRAINT [IXQ_EnumNamespace_Name] UNIQUE NONCLUSTERED ([Name]));
-
--- Add enums to namespace
-INSERT INTO [EnumNamespace] SELECT 'Roadsigns'
-INSERT INTO [EnumNamespace] SELECT 'RouteFlatness'
-INSERT INTO [EnumNamespace] SELECT 'HillType'
-INSERT INTO [EnumNamespace] SELECT 'ForestDensity';
-
--- Create enum tables
-CREATE TABLE [EnumRoadsigns](
-[EnumNamespaceId] INT NOT NULL,
-[Value] INT NOT NULL,
-[Description] NVARCHAR(100) NOT NULL,
-[OrderBy] INT,
-CONSTRAINT [PK_EnumRoadsigns] PRIMARY KEY CLUSTERED ([EnumNamespaceId], [Value]),
-CONSTRAINT [FK_EnumRoadsigns] FOREIGN KEY ([EnumNamespaceId]) REFERENCES [EnumNamespace] ([Id]))
-
-CREATE TABLE [EnumRouteFlatness](
-[EnumNamespaceId] INT NOT NULL,
-[Value] INT NOT NULL,
-[Description] NVARCHAR(100) NOT NULL,
-[OrderBy] INT,
-CONSTRAINT [PK_EnumRouteFlatness] PRIMARY KEY CLUSTERED ([EnumNamespaceId], [Value]),
-CONSTRAINT [FK_EnumRouteFlatness] FOREIGN KEY ([EnumNamespaceId]) REFERENCES [EnumNamespace] ([Id]))
-
-CREATE TABLE [EnumHillType](
-[EnumNamespaceId] INT NOT NULL,
-[Value] INT NOT NULL,
-[Description] NVARCHAR(100) NOT NULL,
-[OrderBy] INT,
-CONSTRAINT [PK_EnumHillType] PRIMARY KEY CLUSTERED ([EnumNamespaceId], [Value]),
-CONSTRAINT [FK_EnumHillType] FOREIGN KEY ([EnumNamespaceId]) REFERENCES [EnumNamespace] ([Id]))
-
-CREATE TABLE [EnumForestDensity](
-[EnumNamespaceId] INT NOT NULL,
-[Value] INT NOT NULL,
-[Description] NVARCHAR(100) NOT NULL,
-[OrderBy] INT,
-CONSTRAINT [PK_EnumForestDensity] PRIMARY KEY CLUSTERED ([EnumNamespaceId], [Value]),
-CONSTRAINT [FK_EnumForestDensity] FOREIGN KEY ([EnumNamespaceId]) REFERENCES [EnumNamespace] ([Id]));
-
--- Fill enum tables
-INSERT INTO [EnumRoadsigns] SELECT 1, 1, 'None', 1
-INSERT INTO [EnumRoadsigns] SELECT 1, 2, 'Some', 2
-INSERT INTO [EnumRoadsigns] SELECT 1, 3, 'Many', 3
-
-INSERT INTO [EnumRouteFlatness] SELECT 1, 1, 'Flat', 1
-INSERT INTO [EnumRouteFlatness] SELECT 1, 2, 'Bumpy', 2
-
-INSERT INTO [EnumHillType] SELECT 1, 1, 'None', 1
-INSERT INTO [EnumHillType] SELECT 1, 2, 'Small', 2
-INSERT INTO [EnumHillType] SELECT 1, 3, 'Steep', 3
-
-INSERT INTO [EnumForestDensity] SELECT 1, 1, 'None', 1
-INSERT INTO [EnumForestDensity] SELECT 1, 2, 'Thin', 2
-INSERT INTO [EnumForestDensity] SELECT 1, 3, 'Thick', 3;
-
--- Create Preferences table
-CREATE TABLE [dbo].[Preferences](
-[Id] INT IDENTITY (1, 1),
-[Length] INT NOT NULL,
-[HillType] INT NOT NULL,
-[Marshiness] BIT NOT NULL,
-[ForestDensity] INT NOT NULL,
-[RouteFlatness] INT NOT NULL,
-[RouteAsphalted] BIT NOT NULL,
-[RouteHardened] BIT NOT NULL,
-[Roadsigns] INT NOT NULL,
-CONSTRAINT [PK_Preferences] PRIMARY KEY ([Id]));
-
--- Fill Preferences table
-INSERT INTO [Preferences] SELECT 1, 2, 0, 1, 1, 0, 0, 2
-INSERT INTO [Preferences] SELECT 100, 3, 1, 3, 2, 0, 0, 1;
+-- Delete allen!
+DROP TABLE [RouteSequence];
+DROP TABLE [POI];
+DROP TABLE [Preferences];
+DROP TABLE [Route];
+DROP TABLE [User];
 
 -- Create User table
 CREATE TABLE [dbo].[User](
@@ -85,33 +13,86 @@ CREATE TABLE [dbo].[User](
 [Name] NVARCHAR(100) NOT NULL,
 [Username] NVARCHAR(100) NOT NULL,
 [Password] NVARCHAR(100) NOT NULL,
-[Preferences] INT NOT NULL,
-CONSTRAINT [PK_User] PRIMARY KEY ([Id]),
-CONSTRAINT [FK_User] FOREIGN KEY ([Preferences]) REFERENCES [Preferences] ([Id]));
+CONSTRAINT [PK_User] PRIMARY KEY ([Id])
+);
 
 -- Fill User table
-INSERT INTO [User] SELECT 'Justin', 'Justintje', 'abc', 1
-INSERT INTO [User] SELECT 'Roy', 'Roytje', 'def', 2;
+INSERT INTO [User] SELECT 'Justin', 'Justintje', 'abc';
+INSERT INTO [User] SELECT 'Roy', 'Roytje', 'def';
+INSERT INTO [User] SELECT 'Typ', 'Typ', 'ghi';
 
--- Create Route table
-CREATE TABLE [dbo].[Route](
+-- Create Preferences table
+CREATE TABLE [dbo].[Preferences](
 [Id] INT IDENTITY (1, 1),
-[Name] NVARCHAR(100) NOT NULL,
-[Owner] INT NOT NULL,
-[StartLocation] NVARCHAR(100),
-[EndLocation] NVARCHAR(100),
-[Difficulty] INT NOT NULL,
+[OwnerId] INT NOT NULL,
 [Length] INT NOT NULL,
 [HillType] INT NOT NULL,
 [Marshiness] BIT NOT NULL,
 [ForestDensity] INT NOT NULL,
 [RouteFlatness] INT NOT NULL,
 [RouteAsphalted] BIT NOT NULL,
-[RouteHardened] BIT NOT NULL,
 [Roadsigns] INT NOT NULL,
+CONSTRAINT [PK_Preferences] PRIMARY KEY ([Id]),
+CONSTRAINT [FK_Preferences] FOREIGN KEY ([OwnerId]) REFERENCES [User] ([Id])
+);
+
+-- Fill Preferences table
+INSERT INTO [Preferences] SELECT 1, 1, 0, 0, 0, 0, 0, 0;
+INSERT INTO [Preferences] SELECT 2, 2, 1, 1, 1, 1, 1, 1;
+
+-- Create Route table
+CREATE TABLE [dbo].[Route](
+[Id] INT IDENTITY (1, 1),
+[OwnerId] INT NOT NULL,
+[Difficulty] INT NOT NULL,
+[Name] NVARCHAR(100) NOT NULL,
+[Length] DECIMAL(9,6) NOT NULL,
+[StartLong] DECIMAL(9,6) NOT NULL,
+[StartLat] DECIMAL(9,6) NOT NULL,
+[EndLong] DECIMAL(9,6) NOT NULL,
+[EndLat] DECIMAL(9,6) NOT NULL,
+[Marshiness] BIT NOT NULL,
+[RouteAsphalted] BIT NOT NULL,
+[HillType] INT NOT NULL,
+[ForestDensity] INT NOT NULL,
+[RouteFlatness] INT NOT NULL,
+[RoadSigns] INT NOT NULL,
 CONSTRAINT [PK_Route] PRIMARY KEY ([Id]),
-CONSTRAINT [FK_Route] FOREIGN KEY ([Owner]) REFERENCES [User] ([Id]));
+CONSTRAINT [FK_Route] FOREIGN KEY ([OwnerId]) REFERENCES [User] ([Id])
+);
 
 -- Fill Route table
-INSERT INTO [Route] SELECT 'Ez Route', 1, NULL, NULL, 1, 1, 1, 0, 1, 1, 1, 1, 3
-INSERT INTO [Route] SELECT 'FAK Route', 2, NULL, NULL, 10, 100, 3, 1, 3, 2, 0, 0, 1; 
+INSERT INTO [Route] SELECT 1, 3, 'Makkelijke route door de 123-laan', 3, 5.979499, 50.888174, 5.931160, 50.879153,		0, 1, 2, 1, 1, 1;
+INSERT INTO [Route] SELECT 2, 5, 'Middelmatige route door het abc-pad', 7.5, 4.979499, 60.888174, 4.931160, 60.879153,	0, 2, 2, 2, 1, 1;
+INSERT INTO [Route] SELECT 3, 7, 'Moeilijke route door de !@#-straat', 11.75, 3.979499, 70.888174, 4.931160, 70.879153, 1, 2, 2, 2, 1, 2;
+
+-- Create POI table
+CREATE TABLE [dbo].[POI](
+[Id] INT IDENTITY(1,1),
+[RouteId] INT NOT NULL,
+[Name] NVARCHAR(100) NOT NULL,
+[Description] NVARCHAR(300) NOT NULL,
+[Long] DECIMAL(9,6) NOT NULL,
+[Lat] DECIMAL(9,6) NOT NULL,
+CONSTRAINT [PK_POI] PRIMARY KEY ([Id]),
+CONSTRAINT [FK_POI] FOREIGN KEY ([RouteId]) REFERENCES [Route] ([Id])
+);
+
+-- Fill POI table
+INSERT INTO [POI] SELECT 1, 'POI van Makkelijke route door de 123-laan', 'Dit is een heel mooie steen.', 5.976499, 50.999174;
+
+-- Create RouteSequence table
+CREATE TABLE [dbo].[RouteSequence](
+[Id] INT IDENTITY(1,1),
+[RouteId] INT NOT NULL,
+[Number] INT NOT NULL,
+[Long] DECIMAL(9,6) NOT NULL,
+[Lat] DECIMAL(9,6) NOT NULL,
+CONSTRAINT [PK_RouteSequence] PRIMARY KEY ([Id]),
+CONSTRAINT [FK_RouteSequence] FOREIGN KEY ([RouteId]) REFERENCES [Route] ([Id])
+);
+
+-- Fill RouteSequence Table
+INSERT INTO [RouteSequence] SELECT 1, 1, 5.976499, 50.999174;
+INSERT INTO [RouteSequence] SELECT 1, 2, 5.986499, 51.0;
+INSERT INTO [RouteSequence] SELECT 1, 3, 5.996499, 51.1;
